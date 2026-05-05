ab37dadac4a7c8f87401f9b12094bb1522e13f96
Frhanarka <frhanarka@gmail.com>
2026-05-05 10:34:49 +0700

    public

--- praktikum.cpp
+++ praktikum.cpp
@@ -8,3 +8,12 @@ class LayangLayang {
 private:
     double d1, d2;
     double s1, s2;
+
+public:
+    void input() {
+        cout << "=== Layang-Layang ===" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi 1: "; cin >> s1;
+        cout << "Sisi 2: "; cin >> s2;
+    }
