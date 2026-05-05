a2fb1b0583cbb9cdb026df90fa5b825220df6ed8
Frhanarka <frhanarka@gmail.com>
2026-05-05 10:36:24 +0700

    public dan double

--- praktikum.cpp
+++ praktikum.cpp
@@ -37,3 +37,19 @@ public:
 class BelahKetupat {
 private:
     double d1, d2, sisi;
+
+public:
+    void input() {
+        cout << "\n=== Belah Ketupat ===" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi: "; cin >> sisi;
+    }
+
+    double luas() {
+        return 0.5 * d1 * d2;
+    }
+
+    double keliling() {
+        return 4 * sisi;
+    }
